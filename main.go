f2e033074c7cb36207d25c41ffedcbbc4fb7c165
t4lz <t4lz.git@gmail.com>
2022-10-13 19:10:12 +0200

    Working on Linux.

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 // #include <stdio.h>
 // void placeholder_c_func() {
-//   printf("Hello, Cgo!");
+//   printf("Cgo: placeholder_c_func\n");
 // }
 // void (*const const_c_func)() = &placeholder_c_func;
 // void bridge_c_func() {
+//   printf("Cgo: bridge_c_func\n");
 //   (*const_c_func)();
 // }
 import "C"
+import (
+	"fmt"
+)
 
 func CallC() {
+	fmt.Println("Go: CallC")
 	C.bridge_c_func()
 }
 
 func main() {
+	fmt.Println("Go: main")
 	CallC()
 }
