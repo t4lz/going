d00e8532a34be71861f8ac393fd5a621d0c78aee
t4lz <t4lz.git@gmail.com>
2022-10-24 14:51:12 +0200

    Get go detour address less stupidly.

--- go-detour/main.go
+++ go-detour/main.go
@@ -4,8 +4,7 @@ package main
 import "C"
 import (
 	"fmt"
-	"strconv"
-	"unsafe"
+	"reflect"
 )
 
 func main() {}
@@ -21,19 +20,12 @@ func detour() {
 
 //export Initialize
 func Initialize(c_detour C.callback) uintptr {
-	detour_func := detour
-	detourPtr := *(*uintptr)(unsafe.Pointer(&detour_func))
+	detourFunc := reflect.ValueOf(detour)
+	detourPtr := detourFunc.Pointer()
 	fmt.Println("Initialize (Go)")
-	fmt.Println("Go detour address (func value):\t\t", detour)
-	fmt.Printf("Go detour address (with printf):\t%p\n", detour)
-	fmt.Printf("Go detour address (uintptr):\t\t0x%x\n", detourPtr)
-
-	s := fmt.Sprintf("%p", detour)
-	res, _ := strconv.ParseUint(s[2:], 16, 64)
-	ptr := uintptr(res)
-	fmt.Printf("Go detour address (parsed string):\t0x%x\n", ptr)
-
+	fmt.Println("Go detour address:\t", detour)
+	fmt.Printf("Go detour address:\t 0x%x\n", detourPtr)
 	fmt.Printf("C (Rust) detour: %p\n", c_detour)
 	global_func_ptr = c_detour // Make the go detour call the c-ABI detour.
-	return ptr
+	return detourPtr
 }
