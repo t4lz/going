bd5d20c3c823ddd995cd991c9df94c6b68aebda3
t4lz <t4lz.git@gmail.com>
2022-11-02 15:14:21 +0100

    Crash.

--- go-detour/main.go
+++ go-detour/main.go
@@ -15,8 +15,16 @@ var global_func_ptr C.syscall_callback
 
 //go:noinline
 func syscallDetour(trap, arg1, arg2, arg3 uintptr) (uintptr, uintptr, syscall.Errno) {
-	res := C.syscall_bridge(global_func_ptr, C.uintptr_t(trap), C.uintptr_t(arg1), C.uintptr_t(arg2), C.uintptr_t(arg3))
-	return uintptr(res.res1), uintptr(res.res2), syscall.Errno(res.errno)
+	res1 := make(chan uintptr)
+	res2 := make(chan uintptr)
+	errno := make(chan syscall.Errno)
+	go func(res1 chan uintptr, res2 chan uintptr, errno chan syscall.Errno) {
+		res := C.syscall_bridge(global_func_ptr, C.uintptr_t(trap), C.uintptr_t(arg1), C.uintptr_t(arg2), C.uintptr_t(arg3))
+		res1 <- uintptr(res.res1)
+		res2 <- uintptr(res.res2)
+		errno <- syscall.Errno(res.errno)
+	}(res1, res2, errno)
+	return <-res1, <-res2, <-errno
 }
 
 //export Initialize
